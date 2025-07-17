91c55ebb654aea47c4ddabb22f7124989a12f559
devanshi <parekhdevanshi76@gmail.com>
2025-07-17 18:49:55 +0530

    updated

--- PR-5-Basic Calculation of Array/methods-of-array/employee.js
+++ PR-5-Basic Calculation of Array/methods-of-array/employee.js
@@ -33,11 +33,43 @@ let employee = [
     },
 ];
 
+// push()
+
+employee.push({
+    id : 6,
+    name : "Riya",
+    email : "riya@gmail.com",
+    post : "backend devloper",
+})
+
+console.log(employee);
+
+// pop()
+
+employee.pop();
+
+console.log(employee);
+
+// map()
+
+let allNames = employee.map(emp => emp.name);
+console.log("All Names:", allNames);
+
+// reverse
+
+let sortdata = employee.sort((a,b) => a-b);
+let reversedata = employee.reverse();
+
+// find()
+
+let findemail = employee.find((emp) => emp.email === "devanshi@gmail.com");
+console.log("User by email : ",findemail);
+
+// filter()
+
 let filteruser = employee.filter((employee) => {
     return employee.name === "Diya";
 });
-
-console.log(employee);
 
 console.log("----------------------------------------------")
 console.log("Data Found : ")
